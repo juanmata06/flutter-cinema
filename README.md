122d72d088a8074ae122a073347829c2f7207102
juanmata06 <mataguzzojuan@gmail.com>
2025-03-25 16:45:15 +0100

    isar.dev installed

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 # Dear dev, follow these steps please:
 1. Copy .env.template and rename it as .env
 2. Write your TMDB api key
+3. For every change in entities, please run
+```
+flutter pub run build_runner build
+```
--- lib/domain/entities/movie.dart
+++ lib/domain/entities/movie.dart
@@ -1,4 +1,11 @@
+import 'package:isar/isar.dart';
+
+part 'movie.g.dart';
+
+@collection
 class Movie {
+  Id isarId = Isar.autoIncrement; // id for Isar database
+  
   final bool adult;
   final String backdropPath;
   final List<String> genreIds;
