d322ef1c4a73226cc4f81f8f3379fb21bbf52f10
juanmata06 <mataguzzojuan@gmail.com>
2025-03-31 15:38:31 +0200

    app finished

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:flutter_cinema/presentation/provider/providers_exports.dart';
 import 'package:flutter_cinema/presentation/widgets/widgets_exports.dart';
 
@@ -34,6 +35,34 @@ class FavoritesViewState extends ConsumerState<FavoritesView> {
   @override
   Widget build(BuildContext context) {
     final favoriteMovies = ref.watch(favoriteMoviesProvider).values.toList();
+    final colors = Theme.of(context).colorScheme;
+
+    if(favoriteMovies.isEmpty){
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(Icons.favorite_outline_sharp, size: 60, color: colors.primary),
+            Text(
+              'No favorites movies yet',
+              style: TextStyle(fontSize: 30, color: colors.primary),
+              textAlign: TextAlign.center,
+            ),
+            const Text(
+              'Click on movies like button for save them here.',
+              style: TextStyle(fontSize: 20, color: Colors.black),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            FilledButton.tonal(
+              onPressed: () => context.go('/home/0'), 
+              child: const Text('Look for movies')
+            )
+          ],
+        ),
+      );
+    }
 
     return Scaffold(
       body: Column(
